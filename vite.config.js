041a32fe53ea5943d0925e35fda90ad5981e2b79
rajviCE <desai.rajvi8@gmail.com>
2025-04-27 01:25:41 +0530

    Added PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig(
   {
     server: {
       host: true
     },
-  plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Expenza',
+          short_name: 'Expenza',
+          description: 'Family Expense Tracker',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          display: 'standalone',
+          icons: [
+            {
+              src: '/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
+    ]
 })
